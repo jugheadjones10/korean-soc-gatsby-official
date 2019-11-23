81f29920a11295ba1d61eca813bef410fe0a87cc
Kim Young Jin <kimyoungjin1001@gmail.com>
2019-11-24 02:11:06 +0800

    YJ first commit

---
+++ src/components/header-footer.js
@@ -0,0 +1,83 @@
+import React from "react"
+
+// Styles (universal styles to be passed to all children components)
+import "bootstrap/dist/css/bootstrap.min.css"
+import "../styles/global.css"
+
+// Component-specific style
+import headFootStyles from "./header-footer.module.css"
+
+// React-bootstrap
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from "react-bootstrap/Nav"
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+
+// Assets
+import logo from "./images/korea-society-logo.jpg"
+
+export default ({ children }) => (
+  <div>
+    <header className={`${headFootStyles.navigation} fixed-top`}>
+      <Navbar collapseOnSelect expand="md">
+        <Navbar.Brand href="#home">
+          <img
+            src={logo}
+            width="50"
+            height="50"
+            alt="NUS Korean Society logo"
+          />
+        </Navbar.Brand>
+
+        <Navbar.Toggle />
+
+        <Navbar.Collapse className="justify-content-end text-center">
+          <Nav activeKey="#home">
+            <Nav.Item>
+              <Nav.Link className={headFootStyles.navLink} href="#home">Home</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link className={headFootStyles.navLink} href="#about-society">한인회 소개</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link className={headFootStyles.navLink} href="#about-school">학교 소개</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link className={headFootStyles.navLink} href="#contact">연락</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </header>
+
+    {children}
+
+    <footer className={headFootStyles.bgDark}>
+      <div className="section">
+        <Container>
+          <Row>
+            <Col md={4}>
+              <h5 className="text-secondary d-flex justify-content-center">Email</h5>
+              <p className="text-white paragraph-lg font-secondary d-flex justify-content-center">
+                steve.fruits@email.com
+              </p>
+            </Col>
+            <Col md={4}>
+              <h5 className="text-secondary d-flex justify-content-center">Phone</h5>
+              <p className="text-white paragraph-lg font-secondary d-flex justify-content-center">
+                +880 2544 658 256
+              </p>
+            </Col>
+            <Col md={4}>
+              <h5 className="text-secondary d-flex justify-content-center">Address</h5>
+              <p className="text-white paragraph-lg font-secondary d-flex justify-content-center text-center">
+                21 Lower Kent Ridge Rd, Singapore 119077
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </footer>
+  </div>
+)
---
+++ src/pages/index.js
@@ -0,0 +1,192 @@
+import React from "react"
+
+// Components
+import HeaderFooter from "../components/header-footer"
+import PhotoGrid from "../components/photogrid"
+
+// Styles
+import indexStyles from  "./index.module.css"
+
+// React-bootstrap
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Card from "react-bootstrap/Card"
+
+// Assets
+import twitter from "../components/images/twitter.svg"
+import facebook from "../components/images/facebook.svg"
+import instagram from "../components/images/instagram.svg"
+import modified_logo from "../components/images/korea-society-logo-modified.jpg"
+import medal from "../components/images/medall.svg"
+import basketball from "../components/images/basketball.svg"
+import camera from "../components/images/camera.svg"
+import educatin_bg from "../components/images/education-bg.png"
+
+export default () => (
+    <div>
+        <HeaderFooter>
+
+            {/* Hero area */}
+            <section className={`${indexStyles.heroArea} bg-white px-5 position-relative`}>
+                <Container>
+                    <Row>
+                        <Col md={6} className="my-5 my-md-0 d-flex align-items-center">
+                            <h1>NUS<br/>한인회</h1>
+                        </Col>
+                        <Col md={6} className="d-none d-md-flex justify-content-end">
+                           <img alt="korean society lion" width="400" src={modified_logo}></img>
+                        </Col>
+                    </Row>
+                </Container>
+                <ul className="ml-5 mt-3 list-unstyled">
+                    <li className="mb-3"><a href="https://www.facebook.com/groups/258391824278076/"><img alt="facebook" src={facebook}/></a></li>
+                    <li className="mb-3"><a href="#"><img alt="instagram" src={instagram}/></a></li>
+                    <li className="mb-3"><a href="#"><img alt="twitter" src={twitter}/></a></li>
+                </ul>
+                <img className="img-fluid w-100 edu-bg-image" src={educatin_bg} alt="bg-image"/>
+            </section>
+            {/* Hero area */}
+
+                        
+            {/* Introduction */}
+            <section className="section">
+                <Container>
+                    <Row>  
+                        <Col lg={12} className="text-center">
+                            <h2 className="section-title">인사말</h2>
+                        </Col>
+
+                        <Col lg={10} className="mx-auto text-center">
+                            <p className="font-secondary paragraph-lg text-dark">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                                do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+                                in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                            </p>
+                            {/* <a href="about.html" class="btn btn-transparent">more</a> */}
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+            {/* Introduction */}
+
+
+            {/* History */}
+            <section className="section position-relative">
+                <Container>
+                    <Row>
+                        <Col lg={12} className="text-center">
+                            <h2 className="section-title">역사</h2>
+                        </Col>
+                        <Col lg={6} md={6} className={indexStyles.mb80}>
+                            <div className="d-flex">
+                                <div className="mr-lg-5 mr-3">
+                                    <img className="shadow d-block rounded-circle bg-white p-3" width="100" src={medal} />
+                                </div>
+                                <div>
+                                    <p className="text-dark mb-1">2006</p>
+                                        <h4>Marters in UX Design</h4>
+                                    <p className="mb-0 text-muted">Masassusets Institute of Technology</p>
+                                </div>
+                            </div>
+                        </Col>
+                         <Col lg={6} md={6} className={indexStyles.mb80}>
+                            <div className="d-flex">
+                                <div className="mr-lg-5 mr-3">
+                                    <img className="shadow d-block rounded-circle bg-white p-3" width="100" src={medal} />
+                                </div>
+                                <div>
+                                    <p className="text-dark mb-1">2004</p>
+                                        <h4>Honours in Fine Arts</h4>
+                                    <p className="mb-0 text-muted">Harvard University</p>
+                                </div>
+                            </div>
+                        </Col>
+                        <Col lg={6} md={6} className={indexStyles.mb80}>
+                            <div className="d-flex">
+                                <div className="mr-lg-5 mr-3">
+                                    <img className="shadow d-block rounded-circle bg-white p-3" width="100" src={medal} />
+                                </div>
+                                <div>
+                                    <p className="text-dark mb-1">2006</p>
+                                        <h4>Marters in UX Design</h4>
+                                    <p className="mb-0 text-muted">Masassusets Institute of Technology</p>
+                                </div>
+                            </div>
+                        </Col>
+                         <Col lg={6} md={6} className={indexStyles.mb80}>
+                            <div className="d-flex">
+                                <div className="mr-lg-5 mr-3">
+                                    <img className="shadow d-block rounded-circle bg-white p-3" width="100" src={medal} />
+                                </div>
+                                <div>
+                                    <p className="text-dark mb-1">2004</p>
+                                        <h4>Honours in Fine Arts</h4>
+                                    <p className="mb-0 text-muted">Harvard University</p>
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+                <img className={`img-fluid w-100 ${indexStyles.eduBgImage}`} src={educatin_bg} alt="bg-image"/>
+            </section>
+            {/* History */}
+
+            {/* Activities */}
+            <section className="section position-relative">   
+                <Container>
+                    <Row>   
+                        <Col xs={12} className="text-center">
+                            <h2 className="section-title">활동</h2>
+                        </Col>
+
+                        <Col md={4} className="mb-4 mb-md-0">
+                            <Card className={`${indexStyles.hoverShadow} shadow`}>
+                                <Card.Body className="text-center px-4 py-5">
+                                    <img className="mb-5 d-inline-block" width="50" src={medal} />
+                                    <h4 className="mb-4">술</h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                                        dolore magna aliqua.
+                                    </p>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col md={4} className="mb-4 mb-md-0">
+                            <Card className={`${indexStyles.hoverShadow} shadow`}>
+                                <Card.Body className="text-center px-4 py-5">
+                                    <img className="mb-5 d-inline-block" width="50" src={camera} />
+                                    <h4 className="mb-4">활동</h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                                        dolore magna aliqua.
+                                    </p>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col md={4} className="mb-4 mb-md-0">
+                            <Card className="hover-shadow shadow">
+                                <Card.Body className="text-center px-4 py-5">
+                                    <img className="mb-5 d-inline-block" width="50" src={basketball} />
+                                    <h4 className="mb-4">운동</h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                                        dolore magna aliqua.
+                                    </p>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
+                <img className={`img-fluid w-100 ${indexStyles.eduBgImage}`} src={educatin_bg} alt="bg-image"/>
+
+            </section>
+            {/* Activities */}
+
+            {/* Pictures */}
+            <PhotoGrid/>
+            {/* Pictures */}
+
+        </HeaderFooter>
+    </div>
+)
