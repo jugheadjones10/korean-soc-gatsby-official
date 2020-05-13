19128ee2f4c7166a8226a8bd933d3ffc280857c5
Kim Young Jin <kimyoungjin1001@gmail.com>
2020-05-13 22:16:58 +0800

    AWD

--- src/components/header-footer.js
+++ src/components/header-footer.js
@@ -62,6 +62,7 @@ class HeaderFooter extends React.Component {
     return (
       <div>
         <Helmet>
+          <title>NUS Korea Society</title>
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
